af1d4d84d30d94f19b7800a34ebabaf74a4ce1b0
bagrounds <bagrounds@gmail.com>
2017-03-02 01:59:57 -0800

    [MINOR][CHANGE] predicate implementation

--- src/predicate.js
+++ src/predicate.js
@@ -6,35 +6,75 @@
   var optionsChecker = require('./spec-options-predicate')
 
   /* exports */
-  module.exports = predicate
+  module.exports = Predicate
+
+  const ifThenElse = module.exports.ifThenElse = (predicate, ifTrue, ifFalse) =>
+    subject => predicate(subject) ? ifTrue(subject) : ifFalse(subject)
+
+  const predicate = module.exports.predicate = {
+    and: (p1, p2) => subject => p1(subject) && p2(subject),
+    or: (p1, p2) => subject => p1(subject) || p2(subject),
+    not: p => subject => !p(subject)
+  }
+
+  const toString = module.exports.toString = {
+    and: (p1, p2) => () => '(' + p1() + ' AND ' + p2() + ')',
+    or: (p1, p2) => () => '(' + p1() + ' OR ' + p2() + ')',
+    not: p => () => 'NOT(' + p() + ')'
+  }
 
   /**
    *
-   * @function module:fun-predicate.predicate
+   * @constructor module:fun-predicate.Predicate
    *
-   * @param {Object} options all function parameters
-   * @param {*} [options.reference] to compare subject with
-   * @param {Function} options.compare function(subject, reference) -> Boolean
-   * @return {Function} predicate(subject) -> Boolean
+   * @param {Object} options - all function parameters
+   * @param {Function} options.predicate - subject -> Boolean
+   * @param {Function} options.toString - () -> String
    */
-  function predicate (options) {
-    options = optionsChecker(options)
-
-    function result (subject) {
-      return options.compare(subject, options.reference)
+  function Predicate (options) {
+    if (!this) {
+      return new Predicate(options)
     }
 
-    result.toString = function toString () {
-      var description = stringify(options.compare)
+    this.predicate = options.predicate
+    this.toString = options.toString
+  }
 
-      if (options.reference) {
-        description += ' ' + stringify(options.reference)
-      }
+  Predicate.prototype.and = function (p) {
+    return Predicate({
+      predicate: predicate.and(this.predicate, p.predicate),
+      toString: toString.and(this.toString, p.toString)
+    })
+  }
 
-      return description
+  Predicate.prototype.or = function (p) {
+    return Predicate({
+      predicate: predicate.or(this.predicate, p.predicate),
+      toString: toString.or(this.toString, p.toString)
+    })
+  }
+
+  Predicate.prototype.not = function () {
+    return Predicate({
+      predicate: predicate.not(this.predicate),
+      toString: toString.not(this.toString)
+    })
+  }
+
+  Predicate.prototype.check = function (subject) {
+    return this.predicate(subject)
+  }
+
+  Predicate.prototype.assert = function (subject) {
+    if (!this.predicate(subject)) {
+      throw new Error('Assert: ' + stringify(subject) + ' ' + this.toString())
     }
 
-    return result
+    return subject
+  }
+
+  Predicate.prototype.ifThenElse = function (ifTrue, ifFalse) {
+    return ifThenElse(this.predicate, ifTrue, ifFalse)
   }
 })()
 
